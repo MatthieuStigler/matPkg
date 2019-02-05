fff2cac1ab0aa2378a54eb2d673ae4353b8133dd
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-02-04 16:58:45 -0800

    mat_tidy_coef_plot: initial commit

--- R/broom_tidy.R
+++ R/broom_tidy.R
@@ -20,3 +20,20 @@ mat_lm_means_tidy <-  function(x, val_name = value, clean = TRUE) {
 #' @param df data
 #' @export
 mat_tidy_clean <- function(df) setNames(df, stringr::str_replace(colnames(df), "\\.", "_"))
+
+
+mat_tidy_coef_plot <- function(df, x_var, fill_var, fac_1, fac_2=.) {
+  x_var <- rlang::enquo(x_var)
+  fill_var <- rlang::enquo(fill_var)
+  fac_1 <- rlang::enquo(fac_1)
+  fac_2 <- rlang::enquo(fac_2)
+
+
+  df %>%
+    ggplot(aes(x = !!x_var, y = estimate, fill = !!fill_var)) +
+    geom_col(position = "dodge") +
+    geom_errorbar(aes(ymin =  conf.low, ymax =  conf.high, colour = I("black")),
+                  position = "dodge")+
+    facet_grid(!!fac_1~!!fac_2, scales = "free")
+
+}
