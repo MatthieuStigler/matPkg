fb0dcd8a1ae6d18b2479d0d5c81465bb163f2190
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-02-15 17:56:29 -0800

    add do 2 funs

--- R/utilities.R
+++ R/utilities.R
@@ -1,6 +1,9 @@
-
+#' Convert Nan to NA
+#' @param x Scalar/vector
 #' @export
 nan_to_na <-  function(x) ifelse(!is.finite(x), NA, x)
 
-#'@export 
+#' is_true: vectorized isTRUE
+#' @param x vector
+#'@export
 is_true <-  function(x) map_lgl(x, isTRUE)
