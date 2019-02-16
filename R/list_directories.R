484eeee4ce84d2bb21be8308ac52e7c208d9766b
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-02-15 17:56:44 -0800

    list_dir: correct mistake

--- R/list_directories.R
+++ R/list_directories.R
@@ -5,9 +5,11 @@
 #' @param pattern,recursive passed to list.files
 #'
 #' @export
+#' @examples
+#' mat_list_dir(path = ".", pattern = "R")
 mat_list_dir <- function(path, pattern = "\\.tif", recursive = TRUE) {
-  list.files(path, full.names = TRUE, pattern=pattern, recursive = recursive) %>%
-    tibble(full_path=.data$.) %>%
+  li <- list.files(path, full.names = TRUE, pattern=pattern, recursive = recursive)
+  tibble(full_path=li) %>%
     mutate(filename= basename(.data$full_path)) %>%
     select(-.data$full_path, .data$full_path)
 }
