0c95b147e83a98571cc485a70d4823927d571b0b
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-02-19 12:02:27 -0800

    mat_list_dir: add arg extension

--- R/list_directories.R
+++ R/list_directories.R
@@ -3,13 +3,20 @@
 #'
 #' @param path dir path
 #' @param pattern,recursive passed to list.files
+#' @param add_ext Add column with extension?
 #'
 #' @export
 #' @examples
 #' mat_list_dir(path = ".", pattern = "R")
-mat_list_dir <- function(path, pattern = "\\.tif", recursive = TRUE) {
+mat_list_dir <- function(path, pattern = "\\.tif", recursive = TRUE, add_ext = FALSE) {
   li <- list.files(path, full.names = TRUE, pattern=pattern, recursive = recursive)
-  tibble(full_path=li) %>%
-    mutate(filename= basename(.data$full_path)) %>%
+  res <- tibble(full_path=li) %>%
+    mutate(filename= basename(.data$full_path))
+  if(add_ext) res <-  res %>%
+    mutate(ext = tools::file_ext(filename))
+
+  ## return
+  res %>%
     select(-.data$full_path, .data$full_path)
+
 }
