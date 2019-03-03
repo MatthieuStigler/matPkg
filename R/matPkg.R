54117a9ef5202e904eaac95286c68a5bcd956461
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-03-03 11:03:54 -0800

    Update pkg man, add remotes info

--- R/matPkg.R
+++ R/matPkg.R
@@ -1,6 +1,9 @@
 #' matPkg: Internal functions I use
 #'
-#' to do... can I get a list of functions?
+#' to do... can I get a list of functions? Just document them!
+#'
+#' Should run:
+#' remotes::install_github("MatthieuStigler/matPkg", upgrade = "never")
 #'
 #'
 #' @docType package
