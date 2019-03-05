7b4b3cfcfa28e5cf89cf588f5dde96797fadd3c9
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-03-05 14:24:47 -0800

    add new mat_check_0row

--- R/utilities.R
+++ R/utilities.R
@@ -7,3 +7,12 @@ nan_to_na <-  function(x) ifelse(!is.finite(x), NA, x)
 #' @param x vector
 #'@export
 is_true <-  function(x) map_lgl(x, isTRUE)
+
+#' Check 0 rows
+#' @param df data
+#' @examples
+#' mat_check_0row(df=subset(iris, Species=="aaa"))
+#'@export
+mat_check_0row <-  function(df) {
+  if(nrow(df)==0) "OK" else stop("Does not have z rows!")
+}
