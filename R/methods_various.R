795f0e11617b2ae3905176999652bffe26e876b6
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-08-13 14:10:54 -0700

    various method: data.dframe for output of data

--- R/methods_various.R
+++ R/methods_various.R
@@ -11,3 +11,18 @@ as.character.formula <- function (x, ...) {
   form <- gsub("\\s+", " ", form, perl = FALSE)
   return(form)
 }
+
+
+#' As data frame for data()
+#'
+#' @param x formula
+#' @param \ldots unused
+#' @param stringsAsFactors cf \code{\link{as.data.frame}}
+#' @examples
+#' datas <-  data()
+#' head(as.data.frame(datas))
+#' @export
+as.data.frame.packageIQR <-  function(x, ..., stringsAsFactors = FALSE) {
+  as.data.frame(x$results, ..., stringsAsFactors=stringsAsFactors)
+}
+
