cee8ed176ccd11bc05bc0e986ca5388995f158ed
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-08-12 17:52:44 -0700

    mat show_mem: add option for df or list

--- R/memeory_show_check.R
+++ R/memeory_show_check.R
@@ -1,14 +1,41 @@
 #' Show memory usage, by object
+#' @param df_input input df. If empty, will evaluate ls()
 #' @export
 #' @examples
 #' mat_show_mem()
-mat_show_mem <-  function() {
-  tibble(obj = ls(.GlobalEnv)) %>%
-    mutate(obj_size = map(.data$obj, ~pryr::object_size(get(.))),
-           size = map_dbl(.data$obj_size, ~.),
+#' mat_show_mem(freeny)
+#' library(purrr)
+#' library(dplyr)
+#' library(tidyr)
+#'
+#' iris_regs <- nest(iris, data=-Species) %>%
+#'   mutate(reg_out = map(data, ~lm(Petal.Width~Petal.Length, data=as_tibble(.)))) %>%
+#'   select(-data)
+#' mat_show_mem(iris_regs$reg_out)
+mat_show_mem <-  function(df_input) {
+
+  if(missing(df_input)) {
+    df_size <- tibble(object = ls(.GlobalEnv)) %>%
+      mutate(obj_size = map(.data$object, ~pryr::object_size(get(., envir = .GlobalEnv)))) %>%
+      mutate(class_first = map_chr(.data$object, ~ class(get(., envir = .GlobalEnv))[1]))
+  } else {
+    if(is.data.frame(df_input)) {
+      nam <- colnames(df_input)
+    } else if(is.list(df_input)){
+      nam <- names(df_input)
+      if(is.null(nam)) nam <- paste("object", 1:length(df_input), sep="_")
+    }
+    df_size <- tibble(object = nam,
+                      class_first = map_chr(df_input, ~class(.)[1]),
+                      obj_size = map(df_input , pryr::object_size))
+  }
+
+  df_size %>%
+    mutate(object_size = map_dbl(.data$obj_size, ~.),
            unit = map_chr(.data$obj_size, ~class(.)),
-           class_1 = map_chr(.data$obj, ~ class(get(.))[1])) %>%
-    arrange(desc(.data$size))
+           size = map_chr(.data$obj_size, ~utils::capture.output(.))) %>%
+    arrange(desc(.data$object_size)) %>%
+    select(.data$object, .data$size, .data$object_size, .data$unit, .data$class_first)
 }
 
 
