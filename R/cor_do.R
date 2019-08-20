be8aaea4d446853ce2b47f40414a2e4e86aec593
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-08-20 15:59:32 -0700

    do_cor: add_stars argument, format pv aluers

--- R/cor_do.R
+++ R/cor_do.R
@@ -1,23 +1,38 @@
 #' compute correlations among all variables
 #'
 #' @param df data
-#' @param \ldots further argumetns passed to \code{\link{cor.test}}
+#' @param add_stars Add column \code{p_value_chr} with stars?
+#' @param digits if \code{add_stars}, digits to round?
+#' @param \ldots further arguments passed to \code{\link{cor.test}}
 #' @examples
-#' x <- data.frame(matrix(rnorm(500), ncol=5))
-#' colnames(x) <-  letters[1:5]
+#' x1 <- rnorm(100)
+#' x2 <- 0.2 * x1 + rnorm(100)
+#' x3 <- 0.9 * x1 + rnorm(100, sd=0.1)
+#' x4 <- rnorm(100)
+#' x <- data.frame(x1, x2, x3, x4)
 #'
 #' mat_cor_do(x)
+#' mat_cor_do(x, add_stars = TRUE)[, c(1,2,3,4,5,11)]
 #' mat_cor_do(x, method = "kendall")
-#' mat_cor_do(x, method = "kendall", alternative = "greater")
+#' mat_cor_do(x, method = "kendall", alternative = "greater", add_stars = TRUE)
+#'
+#' ## plot
+#' library(ggplot2)
+#' ggplot(aes(x = variable, y= variable2, fill = estimate), data = mat_cor_do(x)) +
+#'    geom_tile() +
+#'    geom_text(aes(label = estimate_pval)) +
+#'    scale_fill_gradient2()
+
 #' @export
-mat_cor_do <- function(df, ...){
+mat_cor_do <- function(df, add_stars = TRUE, digits = 2, ...){
 
 
   ## get df of variable pairs
   cols <-  colnames(df)
   cols_df <- t(utils::combn(cols, 2)) %>%
-    magrittr::set_colnames(c("variable", "variable2")) %>%
-    as_tibble()
+    magrittr::set_colnames(c("variable2", "variable")) %>%
+    as_tibble() %>%
+    select(.data$variable, .data$variable2)
 
   ## compute cor.test() for each, tidy, unnest
   cor_test_out <- cols_df %>%
@@ -25,7 +40,16 @@ mat_cor_do <- function(df, ...){
                                                                     y=pull(df, y), ...) %>%
                         broom::tidy() %>%
                         mat_tidy_clean())) %>%
-    unnest(.data$cor)
+    unnest(.data$cor) %>%
+    mutate_at(c("variable", "variable2"), ~factor(., ordered= TRUE, levels = cols))
+
+  if(add_stars) {
+    cor_test_out <-  cor_test_out %>%
+      mutate(estimate_pval = paste0(round(.data$estimate, digits),
+                                    stats::symnum(.data$p_value, corr = FALSE, na = FALSE,
+                                                  cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                                                  symbols = c("***", "**", "*", ".", " "))))
+  }
   cor_test_out
 
 }
