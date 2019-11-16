c76844dd05606396577596d20a8588706b6c2d85
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-11-16 15:34:29 -0800

    mat_table_to_pdf: option no clean Tex, allow for output from capture.output

--- R/table_to_pdf.R
+++ R/table_to_pdf.R
@@ -3,7 +3,8 @@
 #' Use tools::texi2pdf
 #'@param x the table input
 #'@param filename output name
-#'@param clean,quiet passed to tools::texi2pdf
+#'@param quiet passed to tools::texi2pdf
+#'@param clean_tex,clean_rest Should remove other files, and tex too?
 #'@param plus additional lattex stuff
 #'@seealso  \code{\link{mat_pdf_to_png}}
 #'@examples
@@ -15,15 +16,19 @@
 #'file.remove("filename_test.pdf")
 #'}
 #'@export
-mat_table_to_pdf <- function(x, filename = "input.tex", clean=TRUE, quiet=TRUE,
-                         plus= "\\usepackage{booktabs}\n\\usepackage{dcolumn}\n\\usepackage{underscore}") {
+mat_table_to_pdf <- function(x, filename = "input.tex",  quiet=TRUE,
+                             clean_tex = TRUE, clean_rest = TRUE,
+                             plus= "\\usepackage{booktabs}\n\\usepackage{dcolumn}\n\\usepackage{underscore}") {
 
   if(inherits(x, "xtable")) {
-    warning("xtable object is actually not chracter, use print")
+    warning("xtable object is actually not character, use print/capture.output")
     x <-  print(x)
   }
   if(stringr::str_detect(filename, "\\.pdf$|\\.png$")) warning("filneame should be raw")
   if(!stringr::str_detect(filename, "\\.tex"))   filename <- paste(filename, ".tex", sep="")
+
+  ## format x if length(x) %>%  1
+  if(length(x)>1) x <-  paste(x, collapse = "\n")
 
   first <- c("\\documentclass[varwidth=\\maxdimen]{standalone}[2011/12/21]",  "\\begin{document}")
   last <- "\\end{document}"
@@ -36,17 +41,17 @@ mat_table_to_pdf <- function(x, filename = "input.tex", clean=TRUE, quiet=TRUE,
   old_wd <- getwd()
   setwd(dirname(filename))
   if(!file.exists(basename(filename))) warning("Oups")
-  try(tools::texi2pdf(basename(filename), quiet=quiet, clean=clean))
+  try(tools::texi2pdf(basename(filename), quiet=quiet, clean=FALSE))
   setwd(old_wd)
 
-  if(clean) {
+  if(any(c(clean_tex, clean_rest))) {
     filename_2  <- stringr::str_remove(filename, "\\..+$")
     all_files <- list.files(path = dirname(filename),
                             pattern=basename(filename_2),
                             full.names = TRUE)
-    # all_files <- list.files(pattern=filename_2)
-    all_files_rem <- all_files[!stringr::str_detect(all_files, "\\.pdf")]
-    file.remove(all_files_rem)
+
+    if(clean_rest) file.remove(all_files[!stringr::str_detect(all_files, "\\.pdf$|\\.tex$")])
+    if(clean_tex) file.remove(all_files[stringr::str_detect(all_files, "\\.tex$")])
   }
 }
 
