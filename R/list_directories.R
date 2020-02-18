98f6921d2d54fd91bd6227e9f0d0f3af53ab55f6
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2020-02-18 11:40:19 -0800

    List directory: test if folder there, allow URL style

--- R/list_directories.R
+++ R/list_directories.R
@@ -4,12 +4,21 @@
 #' @param path dir path
 #' @param pattern,recursive passed to list.files
 #' @param add_ext Add column with extension?
+#' @param \ldots Additional arguments to \code{\link{list.files}}
 #'
 #' @export
 #' @examples
 #' mat_list_dir(path = ".", pattern = "R")
-mat_list_dir <- function(path, pattern = "\\.tif", recursive = TRUE, add_ext = FALSE) {
-  li <- list.files(path, full.names = TRUE, pattern=pattern, recursive = recursive)
+mat_list_dir <- function(path, pattern = "\\.tif", recursive = TRUE, add_ext = FALSE, ...) {
+
+  ## check
+  if(!file.exists(path)) stop("Missing folder?")
+  if(str_detect(path, "%20")) path <- utils::URLdecode(path)
+
+  ## read
+  li <- list.files(path, full.names = TRUE, pattern=pattern, recursive = recursive, ...)
+
+  ## process
   res <- tibble(full_path=li) %>%
     mutate(filename= basename(.data$full_path))
   if(add_ext) res <-  res %>%
