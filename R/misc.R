912044fe98bf577393cdb8666ad9c11dabc99120
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2020-10-01 13:56:21 -0700

    mat_str_to_expr: handle more cases

--- R/misc.R
+++ R/misc.R
@@ -2,11 +2,22 @@
 #'
 #'Simply add annoying ~ as space, and converts as expression
 #' @param x A single charracter string
+#' @param echo Print before eval? For de-bugging
 #' @return An expression
 #' @examples
 #' plot(1)
 #' title(mat_str_to_expr("This is R^2"))
+#' plot(1)
+#' title(mat_str_to_expr(x="This is R^2 in 1%", echo=TRUE))
+#' plot(1)
+#' title(mat_str_to_expr(x="This is R^2 in %", echo=TRUE))
 #' @export
-mat_str_to_expr <- function(x){
-  parse(text=stringr::str_replace_all(x, " ", " ~ "))
+mat_str_to_expr <- function(x, echo=FALSE){
+  str_prep <- str_replace_all(x, " ", "~") %>%
+    str_replace_all("(&|%)", "*'\\1'") %>%
+    str_replace_all("in", "'in'") %>%
+    str_replace_all("\\(\\*", "(") %>%
+    str_replace_all("~\\*", "~")
+  if(echo) print(str_prep)
+  parse(text=str_prep)
 }
