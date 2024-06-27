a7ec2a3756859372f6c181d59e48e45617b2678a
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2024-06-27 11:46:37 +0200

    Change pkg desc

--- R/matPkg.R
+++ R/matPkg.R
@@ -1,12 +1,12 @@
-#' matPkg: Internal functions I use
-#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' A personal package for functions I use often. Use at your own risk, no guarantee of being consistent.
 #'
 #' Should run:
 #'
 #' remotes::install_github("MatthieuStigler/matPkg", upgrade = "never")
-#'
-#'
-#' @docType package
-#' @name matPkg
+## usethis namespace: end
 NULL
+
